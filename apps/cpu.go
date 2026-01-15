add30473a047989313dbb15bd20f132de6506243
daniswastaken <daniswastaken@gmail.com>
2026-01-15 18:21:43 +0700

    removing loops usage in cpu

--- apps/cpu.go
+++ apps/cpu.go
@@ -1,25 +1,22 @@
 package apps
 
 import (
-	"fmt"
-	"github.com/shirou/gopsutil/v4/cpu" // gopsutil package for CPU usage
-	"time"
+    "fmt"
+    "github.com/shirou/gopsutil/v4/cpu"
+    "time"
 )
 
-func CPUInfo() float64{
-	for {
-		cpuUsage, err := cpu.Percent(time.Second, false)
+func CPUInfo() float64 {
+    // 1. Just do the action ONCE
+    cpuUsage, err := cpu.Percent(time.Second, false)
 
-		if err != nil {
-			fmt.Println("Error reading CPU:", err)
-			return 0
-		} // catch CPU error
+    if err != nil {
+        fmt.Println("Error reading CPU:", err)
+        return 0
+    }
 
-        return cpuUsage[0]
-        /*
-		fmt.Printf("\rCPU Usage: %.2f%% | Cores: %d ", cpuUsage[0], cpuCores)
-        */
-	}
+    // 2. Return the result and get out
+    return cpuUsage[0]
 }
 
 func CPUCores() int {
